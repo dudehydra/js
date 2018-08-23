8618108631865c9b094862774c69d205baf2dfb1
dude <dudehydra@gmail.com>
2018-08-23 08:31:02 +0300

    upd task

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const STARTPAGE = `5`;
 let currentScreen = 0;
+const Main = document.querySelector(`.main`);
+let pages = document.querySelector(`#templates`).content.children;
+let container = document.createElement(`template`);
 function changePages(pageNumber) {
-  const Main = document.querySelector(`.main`);
-  let pages = document.querySelector(`#templates`).content.children;
-  let container = document.createElement(`template`);
   container.content.appendChild(pages[pageNumber].cloneNode(true));
   Main.innerHTML = ` `;
   Main.appendChild(container.content);
@@ -15,9 +15,17 @@ function keyPressHandler(evt) {
   const KEY_RIGHT = 39;
   if (evt.altKey) {
     if (evt.keyCode === KEY_LEFT) {
-      changePages(currentScreen - 1);
+      if (currentScreen === 0) {
+        changePages(pages.length - 1);
+      } else {
+        changePages(currentScreen - 1);
+      }
     } else if (evt.keyCode === KEY_RIGHT) {
-      changePages(currentScreen + 1);
+      if (currentScreen === pages.length) {
+        changePages(1);
+      } else {
+        changePages(currentScreen + 1);
+      }
     }
   }
 }
